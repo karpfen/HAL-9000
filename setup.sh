856cf908bab1a6fba2d93997bda9402c06a61e0e
karpfen <andreas@petutschnig.de>
2024-12-12 20:44:08 +0100

    adapt to manjaro

--- setup.sh
+++ setup.sh
@@ -1,48 +1,35 @@
 # Setup for home assistant
 
 # enable ssh access
-sudo apt update
-sudo apt install openssh-server
-sudo ./utils/set_static_ip.sh
-# TODO add to utils/sshd_config
-# PermitRootLogin no
-# AllowUsers your_username
+sudo pacman -S openssh
+sudo systemctl enable --now sshd
 
-# cp utils/sshd_config /etc/ssh/sshd_config
-sudo systemctl restart ssh
+cp utils/sshd_config /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
 sudo ufw allow ssh
 sudo ufw enable
 
 # Install docker
-## Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo pacman -S docker
 
-## Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo setfacl --modify user:andreas:rw /var/run/docker.sock
 
 # pull home-assistant
 docker pull homeassistant/home-assistant
 
 # map config directory to local machine
-mkdir -p /home/home-assistant/config
+mkdir -p ~/home-assistant/config
 # run home-assistant
 docker run -d \
+  --privileged
   --name HAL-9000 \
   --restart=unless-stopped \
   -e TZ=Europe/Berlin \
-  -v /home/home-assistant/config:/config \
+  -v ~/home-assistant/config:/config \
+  -v /run/dbus:/run/dbus:ro \
   --network=host \
   ghcr.io/home-assistant/home-assistant:stable
 
