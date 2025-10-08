26a7da202c532a5128872d1e17979ce57efe88d2
karpfen <andreas@petutschnig.de>
2025-10-08 21:09:33 +0200

    cleanup shellscripts

--- update_hal_9000.sh
+++ update_hal_9000.sh
@@ -5,6 +5,13 @@ docker-compose \
 	-f mealie.yml \
 	-f nextcloud.yml \
 	-f paperless.yml \
+	pull
+
+docker-compose \
+	-f home-assistant.yml \
+	-f mealie.yml \
+	-f nextcloud.yml \
+	-f paperless.yml \
 	up \
 	-d
 
